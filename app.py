5e1faf7f3d951fc7f64aed92da91efc99e9b6b42
Filipe Pacheco <filipe.pacheco@smartnx.local>
2019-06-04 09:57:56 -0300

    test return

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
 
 @app.route("/api")
 def ret():
-    result = [{"id":37823,"name":"J\u00e9ssica Reis"},{"id":37822,"name":"Tadeu Francisco"},{"id":37821,"name":"Matheus Pado"},{"id":37720,"name":"Palloma"},{"id":37191,"name":"Peter Parker"},{"id":37186,"name":"Peter Parker"},{"id":37185,"name":"Flavio Almeida"},{"id":37182,"name":"Filipe Pacheco"},{"id":36985,"name":"Sebasti\u00e3o Jos\u00e9 Almeida Barbosa J\u00fanior"},{"id":36972,"name":"Sucesso do Cliente"},{"id":35976,"name":"\u00cdtalo N\u00f3brega"},{"id":35962,"name":"Paulo Tinoco"},{"id":22341,"name":"Vin\u00edcius Melo"}]
+    result = '{"id":"37182","name":"Filipe P","email":"aa@aa.com","login":"aa@aa.com","type":2,"phone":"32988776655"}'
     return result
 
 if __name__ == "__main__":
