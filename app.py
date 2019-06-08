fae394935e7d67652ecff0cd93b1d6fe2626f2c5
FilipeQuina <filipe.pacheco00@gmail.com>
2019-06-08 15:05:54 -0300

    file system

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, send_file
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Aqui será a tela principal'
 
-@app.route('/teste')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/slide/')
+def download_slide():
+    return send_file(os.getcwd() + os.sep + 'slides'+ os.sep + 'Ageu-001.ppt',  as_attachment=True, attachment_filename = 'Ageu-001.ppt')
+
+app.run(debug=True, use_reloader=True)
