61c569a0156868799ef9a861b07156e610efb0b6
Filipe Pacheco <filipe.pacheco@smartnx.local>
2019-06-12 16:34:18 -0300

    test buildpack

--- gospel.py
+++ gospel.py
@@ -20,7 +20,7 @@ options = Options()
 options.add_argument("--headless")
     
 def createSlide():
-    driver = webdriver.Firefox(options=options, executable_path = os.getcwd() + os.sep + "drive" + os.sep + "geckodriver.exe")
+    driver = webdriver.Firefox(options=options)
     
     verificationErrors = []
     accept_next_alert = True
