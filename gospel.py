7085e7de233c3ffb859f6b60f6ccc1f86595baab
Filipe Pacheco <filipe.pacheco@smartnx.local>
2019-06-19 13:18:44 -0300

    change Selenium for Webcsrap

--- gospel.py
+++ gospel.py
@@ -1,39 +1,34 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import ActionChains
 from pptx import Presentation
 from pptx.util import Pt,Inches
 import os
-import unittest
-import time
-import re
 
-options = Options()
-options.add_argument("--headless")
+def createSlide():
+    html = urlopen("https://www.letras.mus.br/isaias-saad/ousado-amor/")
+    res = BeautifulSoup(html.read(),"lxml")
     
-#def createSlide():
-driver = webdriver.Firefox(options=options, executable_path=os.getcwd()+"/vendor/geckodriver/geckodriver", firefox_binary=os.getcwd()+"/vendor/firefox/")
+    print((res.find_all(name='h1')[1]).text)
+    print((res.find(name='h2').find(name="a").text.strip()))
 
-verificationErrors = []
-accept_next_alert = True
+    titulo = (res.find_all(name='h1')[1]).text
+    subtitulo = (res.find(name='h2').find(name="a").text.strip())
+    lista_paragrafos = (res.find_all(name='p'))
+    print(titulo)
+    print(subtitulo)
+    '''
+    for l in lista_paragrafos:
+        print(type(l))
+        l.replace("<p>","")
+        l.replace("</p>","")
 
-print("VAI")
-lista_musicas = ['https://www.letras.mus.br/anderson-freire/1789576/']
-for lista in lista_musicas:
-    driver.get(lista)
-    titulo = driver.find_element_by_css_selector('.cnt-head_title > h1:nth-child(1)').text
-    subtitulo = driver.find_element_by_css_selector('.cnt-head_title > h2:nth-child(2) > a:nth-child(1)').text
-    lista_paragrafos = driver.find_element_by_css_selector('.cnt-letra').find_elements_by_tag_name('p')
-    #paragrafos = []
-    #for p in lista_paragrafos:
-    #    paragrafos.append(p.text)
-    #    print(p.text)
+        print(l)
+        print("-"*10)
+    '''
     prs = Presentation() 
     slide = prs.slides.add_slide(prs.slide_layouts[0])
     title = slide.shapes.title
@@ -51,7 +46,3 @@ for lista in lista_musicas:
         tf.font.size = Pt(30)
         tf.text = pa.text
     prs.save(r'slides/' + titulo + '-' + subtitulo + ".pptx")
-driver.quit()
-   
-if __name__ == "__main__":
-    unittest.main()
