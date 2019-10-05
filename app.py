c3600f59585df7fe7fc9c284d055e16b56bb0796
FilipeQuina <filipe.pacheco00@gmail.com>
2019-10-05 10:48:11 -0300

    fix names and remove unused functions

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #encoding: utf-8
-from flask import Flask, send_rom_directory, request, render_template, redirect, Response, send_file, make_response
+from flask import Flask, request, render_template, make_response
 import gospel
-import os,io
+import os
 
 app = Flask(__name__)
 
@@ -9,20 +9,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/showLyrics.html')
+@app.route('/showLyrics/')
 def showLyrics():
     musid = request.args.get('musid') 
     return render_template("showLyrics.html",musid=musid)
-
-@app.route('/slide/')
-def download_slide():
-    url = request.args.get("url-link")
-    caminho_slides = os.getcwd() + os.sep + 'slides'
-    for the_file in os.listdir(caminho_slides):
-        if (str(the_file) != ".gitkeep"):
-            os.unlink(caminho_slides + os.sep + the_file)
-    name = gospel.createSlide(url)
-    return send_file(caminho_slides + os.sep + name,  as_attachment=True, attachment_filename = name)
 
 @app.route('/sendByAPI',methods=['POST'])
 def sendByAPI():
@@ -35,15 +25,6 @@ def sendByAPI():
     response.headers['Content-Type'] = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
     response.headers['Content-Description'] = 'attachment; filename={}'.format(name)
     return response
-
-@app.route('/sendByAPI/<name>',methods=['GET'])
-def sendSlide(name):
-    print(name)
-    caminho_slides = os.getcwd() + os.sep + 'slides'
-    try:
-        return send_file(caminho_slides + os.sep + name,  as_attachment=True, mimetype="application/vnd.openxmlformats-officedocument.presentationml.presentation")
-    except Exception as e:
-	    return str(e)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
--- gospel.py
+++ gospel.py
@@ -1,65 +1,12 @@
 import requests
 import os
 import shutil
-import re
 
 from io import BytesIO
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from pptx import Presentation
 from pptx.util import Pt,Inches
-
-def createSlide(url):
-    prs = Presentation() 
-    slide = prs.slides.add_slide(prs.slide_layouts[0])
-    left = Inches(8.2)
-    top = Inches(5.7)
-    html = urlopen(url)
-    res = BeautifulSoup(html.read(),"lxml")
-    titulo = (res.find_all(name='h1')[1]).text
-    subtitulo = (res.find(name='h2').find(name="a").text.strip())
-    letraCompleta = (res.find("div", {"class":'cnt-letra p402_premium'}).find_all(name='p'))
-    title = slide.shapes.title
-    subtitle = slide.placeholders[1]
-    title.text = titulo
-    subtitle.text = subtitulo
-    paragrafos = makeText(letraCompleta)
-
-    for linhas in paragrafos:
-        letra = prs.slides.add_slide(prs.slide_layouts[2]) 
-        shape = letra.shapes
-        left = Inches(8.2)
-        top = Inches(5.7)
-        shape.add_picture("base1.png", left, top)
-        body_shape = shape.placeholders[1]
-        tf = body_shape.text_frame.paragraphs[0]
-        tf.font.size = Pt(40)
-        for linha in linhas:
-            tf.text += "\n"
-            tf.text += str(linha)
-    name_file = titulo + '-' + subtitulo + ".pptx" 
-    prs.save(r'slides/' + name_file)
-    return prs
-
-def makeText(letraCompleta):
-    textClear = []
-    for paragrafos in letraCompleta:
-        l = str(paragrafos).replace("<p>","").replace("</p>","").split("<br/>")
-        for i in range (len(l)):
-            if len(l)>=5:
-                halfText = int(len(l)/2)
-                lineStart = l[i:halfText]
-                lineEnd = l[halfText:int(len(l))]
-                if lineStart not in textClear:
-                    textClear.append(lineStart)
-                if lineEnd not in textClear:
-                    textClear.append(lineEnd)
-                break
-            else:
-                if l not in textClear:
-                    textClear.append(l)
-                break
-    return textClear
 
 def createSlideAPI(name, band, text):
     prs = Presentation() 
@@ -91,5 +38,4 @@ def createSlideAPI(name, band, text):
             tf.text += linhas
     output = BytesIO()
     prs.save(output)
-    teste = output.getvalue()
-    return teste
+    return output.getvalue()
